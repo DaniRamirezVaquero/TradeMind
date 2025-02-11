45a3b72b509e04775d741622cdd74227e246c5a0
Daniel Ramirez <daniel.ramirez@aliqindoi.com>
2025-02-11 17:05:47 +0100

    Add chat component and update app layout

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { ButtonModule } from 'primeng/button';
+import { ChatComponent } from './components/chat/chat.component';
 
 @Component({
   selector: 'app-root',
   imports: [
-    RouterOutlet,
-    ButtonModule
+    ChatComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
